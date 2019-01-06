29abb0567621c1be9b6e72472912319d79866691
gregbown <1952728+gregbown@users.noreply.github.com>
2019-01-06 14:33:56 -0800

    Initial project add

---
+++ src/main/java/studies/springframework/springstudies/SpringStudiesApplication.java
@@ -0,0 +1,14 @@
+package studies.springframework.springstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudiesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/studies/springframework/springstudies/SpringStudiesApplicationTests.java
@@ -0,0 +1,17 @@
+package studies.springframework.springstudies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringStudiesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
